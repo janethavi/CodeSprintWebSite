90db2010513b0ff835344a8ef976132b97632c50
Hasal Fernando <witty.leo98@gmail.com>
2019-04-05 19:48:55 +0530

    Add files via upload

--- karma/checkout.php
+++ karma/checkout.php
@@ -97,38 +97,6 @@
     <!--================Checkout Area =================-->
     <section class="checkout_area section_gap">
         <div class="container">
-            <div class="returning_customer">
-                <div class="check_title">
-                    <h2>Returning Customer? <a href="#">Click here to login</a></h2>
-                </div>
-                <p>If you have shopped with us before, please enter your details in the boxes below. If you are a new
-                    customer, please proceed to the Billing & Shipping section.</p>
-                <form class="row contact_form" action="#" method="post" novalidate="novalidate">
-                    <div class="col-md-6 form-group p_star">
-                        <input type="text" class="form-control" id="name" name="name">
-                        <span class="placeholder" data-placeholder="Username or Email"></span>
-                    </div>
-                    <div class="col-md-6 form-group p_star">
-                        <input type="password" class="form-control" id="password" name="password">
-                        <span class="placeholder" data-placeholder="Password"></span>
-                    </div>
-                    <div class="col-md-12 form-group">
-                        <button type="submit" value="submit" class="primary-btn">login</button>
-                        <div class="creat_account">
-                            <input type="checkbox" id="f-option" name="selector">
-                            <label for="f-option">Remember me</label>
-                        </div>
-                        <a class="lost_pass" href="#">Lost your password?</a>
-                    </div>
-                </form>
-            </div>
-            <div class="cupon_area">
-                <div class="check_title">
-                    <h2>Have a coupon? <a href="#">Click here to enter your code</a></h2>
-                </div>
-                <input type="text" placeholder="Enter coupon code">
-                <a class="tp_btn" href="#">Apply Coupon</a>
-            </div>
             <div class="billing_details">
                 <div class="row">
                     <div class="col-lg-8">
@@ -142,9 +110,6 @@
                                 <input type="text" class="form-control" id="last" name="name">
                                 <span class="placeholder" data-placeholder="Last name"></span>
                             </div>
-                            <div class="col-md-12 form-group">
-                                <input type="text" class="form-control" id="company" name="company" placeholder="Company name">
-                            </div>
                             <div class="col-md-6 form-group p_star">
                                 <input type="text" class="form-control" id="number" name="number">
                                 <span class="placeholder" data-placeholder="Phone number"></span>
@@ -152,13 +117,6 @@
                             <div class="col-md-6 form-group p_star">
                                 <input type="text" class="form-control" id="email" name="compemailany">
                                 <span class="placeholder" data-placeholder="Email Address"></span>
-                            </div>
-                            <div class="col-md-12 form-group p_star">
-                                <select class="country_select">
-                                    <option value="1">Country</option>
-                                    <option value="2">Country</option>
-                                    <option value="4">Country</option>
-                                </select>
                             </div>
                             <div class="col-md-12 form-group p_star">
                                 <input type="text" class="form-control" id="add1" name="add1">
@@ -172,71 +130,42 @@
                                 <input type="text" class="form-control" id="city" name="city">
                                 <span class="placeholder" data-placeholder="Town/City"></span>
                             </div>
-                            <div class="col-md-12 form-group p_star">
-                                <select class="country_select">
-                                    <option value="1">District</option>
-                                    <option value="2">District</option>
-                                    <option value="4">District</option>
-                                </select>
-                            </div>
-                            <div class="col-md-12 form-group">
-                                <input type="text" class="form-control" id="zip" name="zip" placeholder="Postcode/ZIP">
-                            </div>
-                            <div class="col-md-12 form-group">
-                                <div class="creat_account">
-                                    <input type="checkbox" id="f-option2" name="selector">
-                                    <label for="f-option2">Create an account?</label>
-                                </div>
-                            </div>
-                            <div class="col-md-12 form-group">
-                                <div class="creat_account">
-                                    <h3>Shipping Details</h3>
-                                    <input type="checkbox" id="f-option3" name="selector">
-                                    <label for="f-option3">Ship to a different address?</label>
-                                </div>
-                                <textarea class="form-control" name="message" id="message" rows="1" placeholder="Order Notes"></textarea>
-                            </div>
                         </form>
                     </div>
                     <div class="col-lg-4">
                         <div class="order_box">
                             <h2>Your Order</h2>
                             <ul class="list">
-                                <li><a href="#">Product <span>Total</span></a></li>
-                                <li><a href="#">Fresh Blackberry <span class="middle">x 02</span> <span class="last">$720.00</span></a></li>
-                                <li><a href="#">Fresh Tomatoes <span class="middle">x 02</span> <span class="last">$720.00</span></a></li>
-                                <li><a href="#">Fresh Brocoli <span class="middle">x 02</span> <span class="last">$720.00</span></a></li>
+                            <?php
+                                $username = 'Rajitha';
+                                $movie1 = 'Avengers';
+                                $ticketPrice1 = 500;
+                                $tickets1 = 1;
+                                $movie2 = 'Transformers';
+                                $ticketPrice2 = 700;
+                                $tickets2 = 2;
+                                $movie3 = 'The Longest Ride';
+                                $ticketPrice3 = 800;
+                                $tickets3 = 2;
+                                $tickets = 3;
+                                echo '<li><a href="#">Product <span>Total</span></a></li>
+                                <li><a href="#"> '.$movie1.' <span class="middle">x '.$tickets1.'</span> <span class="last">Rs.'.$ticketPrice1*$tickets1.'</span></a></li>
+                                <li><a href="#">'.$movie2.' <span class="middle">x '.$tickets2.'</span> <span class="last">Rs.'.$ticketPrice2*$tickets2.'</span></a></li>
+                                <li><a href="#">'.$movie3.' <span class="middle">x '.$tickets3.'</span> <span class="last">Rs.'.$ticketPrice3*$tickets3.'</span></a></li>
+                                
                             </ul>
                             <ul class="list list_2">
-                                <li><a href="#">Subtotal <span>$2160.00</span></a></li>
-                                <li><a href="#">Shipping <span>Flat rate: $50.00</span></a></li>
-                                <li><a href="#">Total <span>$2210.00</span></a></li>
-                            </ul>
-                            <div class="payment_item">
-                                <div class="radion_btn">
-                                    <input type="radio" id="f-option5" name="selector">
-                                    <label for="f-option5">Check payments</label>
-                                    <div class="check"></div>
-                                </div>
-                                <p>Please send a check to Store Name, Store Street, Store Town, Store State / County,
-                                    Store Postcode.</p>
-                            </div>
-                            <div class="payment_item active">
-                                <div class="radion_btn">
-                                    <input type="radio" id="f-option6" name="selector">
-                                    <label for="f-option6">Paypal </label>
-                                    <img src="img/product/card.jpg" alt="">
-                                    <div class="check"></div>
-                                </div>
-                                <p>Pay via PayPal; you can pay with your credit card if you don’t have a PayPal
-                                    account.</p>
-                            </div>
+                                <li><a href="#">Subtotal <span>Rs. '.(($ticketPrice1*$tickets1)+($ticketPrice2*$tickets2)+($ticketPrice3*$tickets3)).'</span></a></li>
+                                <li><a href="#">Taxes 10% :<span>Rs. '.((($ticketPrice1*$tickets1)+($ticketPrice2*$tickets2)+($ticketPrice3*$tickets3))/10).'</span></a></li>
+                                <li><a href="#">Total <span>Rs. '.((($ticketPrice1*$tickets1)+($ticketPrice2*$tickets2)+($ticketPrice3*$tickets3))+((($ticketPrice1*$tickets1)+($ticketPrice2*$tickets2)+($ticketPrice3*$tickets3))/10)).'</span></a></li>
+                            </ul>';
+                            ?>
                             <div class="creat_account">
                                 <input type="checkbox" id="f-option4" name="selector">
                                 <label for="f-option4">I’ve read and accept the </label>
                                 <a href="#">terms & conditions*</a>
                             </div>
-                            <a class="primary-btn" href="#">Proceed to Paypal</a>
+                            <a class="primary-btn" href="#">Proceed to Pay</a>
                         </div>
                     </div>
                 </div>
@@ -346,3 +275,4 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script>
 </body>
 
 </html>
+
