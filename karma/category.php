6b683fff71524e13b8b93545c3e5f32cd0681d64
janethavi <janethavishka@gmail.com>
2019-04-05 18:25:28 +0530

    Nav Done

--- karma/category.php
+++ karma/category.php
@@ -48,12 +48,12 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
 						<ul class="nav navbar-nav menu_nav ml-auto">
-							<li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
-                            <li class="nav-item active"><a class="nav-link" href="category.html">Shop</a>
+							<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="category.php">Films</a>
 							</li>
-							<li class="nav-item"><a class="nav-link" href="checkout.html">My Cart</a>
+							<li class="nav-item"><a class="nav-link" href="checkout.php">My Cart</a>
 							</li>
-							<li class="nav-item"><a class="nav-link" href="login.html">Sign In</a>
+							<li class="nav-item"><a class="nav-link" href="login.php">Sign In</a>
 							</li>
 						</ul>
 						<ul class="nav navbar-nav navbar-right">
